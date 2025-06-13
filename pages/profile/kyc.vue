2459eedb77bad571eeb336498398a9b42b97ad31
huevangxp <huevang933@gmail.com>
2025-06-13 16:54:51 +0700

    nwe

--- pages/profile/kyc.vue
+++ pages/profile/kyc.vue
@@ -1,38 +1,38 @@
 <template>
     <div class="px-2 my-4">
         <v-row dense>
-            <v-col cols="6">
-                <v-card class="d-flex align-center justify-center" elevation="3" rounded="xl" height="150">
-                    <v-icon size="50" color="primary">mdi-plus</v-icon>
+            <v-col cols="12">
+                <v-card class="d-flex align-center justify-center mx-4" color="#f5f5f5" elevation="0"  rounded="xl" height="200">
+                    <v-icon size="50" color="primary">mdi-image</v-icon>
                 </v-card>
             </v-col>
-            <v-col cols="6" md="6">
-                <v-text-field variant="solo" color="primary" placeholder="ຊື່" elevation="3" rounded="xl"
-                    prepend-inner-icon="mdi-account" class="mx-4" icon-color="primary"></v-text-field>
-                <v-text-field variant="solo" placeholder="ເບິໂທ" color="primary" elevation="3" rounded="xl"
-                    prepend-inner-icon="mdi-cellphone" class="mx-4" icon-color="primary"></v-text-field>
+            <v-col cols="12" >
+                <v-text-field variant="filled" color="primary" placeholder="ຊື່" 
+                    prepend-inner-icon="mdi-account" class="mx-4" ></v-text-field>
+                <v-text-field variant="filled" placeholder="ເບິໂທ" color="primary" 
+                    prepend-inner-icon="mdi-cellphone" class="mx-4" ></v-text-field>
             </v-col>
-            <v-col cols="12" md="6">
-                <v-text-field variant="solo" placeholder="ອີເມວ" color="primary" elevation="3" rounded="xl"
-                    prepend-inner-icon="mdi-email" class="mx-4" icon-color="primary"></v-text-field>
-                <v-textarea variant="solo" placeholder="ທີຢູ່" color="primary" elevation="3" rounded="xl"
-                    prepend-inner-icon="mdi-map-marker" class="mx-4" icon-color="primary"></v-textarea>
+            <v-col cols="12" >
+                <v-text-field variant="filled" placeholder="ອີເມວ" color="primary" 
+                    prepend-inner-icon="mdi-email" class="mx-4" ></v-text-field>
+                <v-textarea variant="filled" placeholder="ທີຢູ່" color="primary" 
+                    prepend-inner-icon="mdi-map-marker" class="mx-4" ></v-textarea>
 
-                <v-text-field variant="solo" disabled placeholder="Beginner" color="primary" elevation="3" rounded="xl"
-                    prepend-inner-icon="mdi-star" class="mx-4" icon-color="primary"></v-text-field>
-                <v-text-field variant="solo" placeholder="ບັດປະຈຳຕົວ" color="primary" elevation="3" rounded="xl"
-                    prepend-inner-icon="mdi-card-account-details" class="mx-4" icon-color="primary"></v-text-field>
-                <v-card class="mx-4 mb-4 d-flex align-center justify-center" height="200" elevation="3" rounded="xl">
+                <v-text-field variant="filled" disabled placeholder="Beginner" color="primary" 
+                    prepend-inner-icon="mdi-star" class="mx-4" ></v-text-field>
+                <v-text-field variant="filled" placeholder="ບັດປະຈຳຕົວ" color="primary" 
+                    prepend-inner-icon="mdi-card-account-details" class="mx-4" ></v-text-field>
+                <v-card class="mx-4 mb-4 d-flex align-center justify-center" color="#f5f5f5" elevation="0" rounded="xl" height="200" >
                     <div class="text-center">
-                        <v-icon size="50" color="primary">mdi-plus</v-icon>
-                        <p>ເລືອກບັດປະຈຳຕົວ</p>
+                        <v-icon size="50" color="primary">mdi-image</v-icon>
+                        <p >ເລືອກບັດປະຈຳຕົວ</p>
                     </div>
                 </v-card>
-                <v-text-field variant="solo" placeholder="ບັດທະນາຄານ" color="primary" elevation="3" rounded="xl"
-                    prepend-inner-icon="mdi-card-account-details" class="mx-4" icon-color="primary"></v-text-field>
-                    <v-card class="mx-4 mb-4 d-flex align-center justify-center" height="200" elevation="3" rounded="xl">
+                <v-text-field variant="filled" placeholder="ບັດທະນາຄານ" color="primary" 
+                    prepend-inner-icon="mdi-card-account-details" class="mx-4" ></v-text-field>
+                    <v-card class="mx-4 mb-4 d-flex align-center justify-center" color="#f5f5f5" elevation="0" rounded="xl" height="200" >
                     <div class="text-center">
-                        <v-icon size="50" color="primary">mdi-plus</v-icon>
+                        <v-icon size="50" color="primary">mdi-image</v-icon>
                         <p>ເລືອກບັດທະນາຄານ</p>
                     </div>
                 </v-card>
@@ -41,8 +41,8 @@
         <div>
             <v-card-actions class="d-flex align-center justify-center">
                 <v-btn color="red" variant="outlined" size="large" width="165" class="rounded-xl"
-                    to="/login">ຍົກເລິກ</v-btn>
-                <v-btn color="primary" variant="elevated" size="large" width="165" class="rounded-xl">ບັນທີກ</v-btn>
+                    to="/login"> <v-icon class="mr-2">mdi-cancel</v-icon> ຍົກເລິກ</v-btn>
+                <v-btn color="primary" variant="elevated" size="large" width="165" class="rounded-xl"> <v-icon class="mr-2">mdi-content-save</v-icon> ບັນທີກ</v-btn>
             </v-card-actions>
         </div>
     </div>
