0e7cf553e76c5a1479124cb3f7ed68bd44eb1cea
huevangxp <huevang933@gmail.com>
2026-01-16 10:01:49 +0700

    commit Fri 01/16/2026 10:01:49.11

--- pages/profile/team.vue
+++ pages/profile/team.vue
@@ -1,29 +1,21 @@
 <template>
   <div class="px-2 my-4">
-    <v-card elevation="3" rounded="xl" class="d-flex align-center">
-      <v-card-title>
-        <v-img
-          src="https://img.freepik.com/premium-photo/portrait-serious-african-guy-looking-camera_116547-37326.jpg?semt=ais_hybrid&w=740"
-          height="150"
-          width="120"
-          cover
-        ></v-img>
-      </v-card-title>
-      <div>
-        <v-card-text>
-          <h2
-            class="text-primary d-flex align-center justify-space-between"
-            style="width: 100%"
-          >
-            <div>ທ່ານ ລີຟອງ ວ່າງ</div>
-          </h2>
-          <h4>ອີເມວ: <span class="text-primary">lisfoom@gmail.com</span></h4>
-          <h4>ເບິໂທ: <span class="text-primary">085555555</span></h4>
-          <h4>ສະຖານະ: <span class="text-primary">VIP</span></h4>
-          <h4>ລູກທິມ: <span class="text-red">100</span> ຄົນ</h4>
-        </v-card-text>
-      </div>
-    </v-card>
+    <!-- <v-card elevation="3" rounded="xl" class="d-flex align-center">
+            <v-card-title>
+                 <v-img src="https://img.freepik.com/premium-photo/portrait-serious-african-guy-looking-camera_116547-37326.jpg?semt=ais_hybrid&w=740" height="150" width="120" cover></v-img>
+            </v-card-title>
+        <div>
+            <v-card-text>
+                <h2 class="text-primary d-flex align-center justify-space-between" style="width: 100%;"><div>ທ່ານ ລີຟອງ ວ່າງ</div> 
+                </h2>
+                <h4>ອີເມວ: <span class="text-primary">lisfoom@gmail.com</span></h4>
+                <h4>ເບິໂທ: <span class="text-primary">085555555</span></h4>
+                <h4>ສະຖານະ: <span class="text-primary">VIP</span></h4>
+                <h4>ລູກທິມ: <span class="text-red">100</span> ຄົນ</h4>
+            </v-card-text>
+          
+        </div>
+        </v-card> -->
 
     <v-card elevation="3" rounded="xl" class="my-4" color="primary" dark>
       <v-card-title class="d-flex align-center justify-space-between">
