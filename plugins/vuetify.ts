2cd1a958a5de37b664827b01be1b56f24cfaa0e0
huevangxp <huevang933@gmail.com>
2026-01-16 08:37:11 +0700

    commit Fri 01/16/2026  8:37:11.11

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,29 +1,30 @@
 // import this after install `@mdi/font` package
-import '@mdi/font/css/materialdesignicons.css'
+import "@mdi/font/css/materialdesignicons.css";
 
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
 
 export default defineNuxtPlugin((app) => {
   const vuetify = createVuetify({
     // ... your configuration
-    // theme code color 
+    // theme code color
     theme: {
-      defaultTheme: 'light',
+      defaultTheme: "light",
       themes: {
         light: {
           colors: {
-            primary: '#2275A8',
-            secondary: '#424242',
-            accent: '#82B1FF',
-            error: '#FF5252',
-            info: '#2196F3',
-            success: '#4CAF50',
-            warning: '#FFC107',
+            // primary: '#2275A8',
+            primary: "#008080",
+            secondary: "#424242",
+            accent: "#82B1FF",
+            error: "#FF5252",
+            info: "#2196F3",
+            success: "#4CAF50",
+            warning: "#FFC107",
           },
         },
       },
     },
-  })
-  app.vueApp.use(vuetify)
-})
+  });
+  app.vueApp.use(vuetify);
+});
