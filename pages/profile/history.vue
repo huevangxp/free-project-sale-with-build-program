302e7a8ffa314d5072b8e0783c3fd81618343b49
huevangxp <huevang933@gmail.com>
2026-01-16 10:44:20 +0700

    commit Fri 01/16/2026 10:44:20.17

--- pages/profile/history.vue
+++ pages/profile/history.vue
@@ -36,7 +36,7 @@
             v-for="(order, i) in filteredOrders"
             :key="i"
             class="mb-3 rounded-xl border-0"
-            elevation="6"
+            elevation="2"
             color="white"
             @click="order.status === 'PAID' ? openBill(order) : null"
             link
