d3ebdebc560d2681d8cdacb76f923c9176087b9d
huevangxp <huevang933@gmail.com>
2026-01-16 10:33:56 +0700

    commit Fri 01/16/2026 10:33:56.17

--- pages/profile/video.vue
+++ pages/profile/video.vue
@@ -58,7 +58,7 @@
         <div class="d-flex align-center mb-3 px-2">
           <v-icon color="primary" class="mr-2">mdi-video-outline</v-icon>
           <h3 class="text-subtitle-1 font-weight-bold text-primary">
-            ລາຍການວິດີໂອ
+            <span>ລາຍການວິດີໂອ</span>
           </h3>
         </div>
 
