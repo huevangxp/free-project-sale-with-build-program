816319c27b50962ce4bbe48d5f1feabb0ffb0df6
huevangxp <huevang933@gmail.com>
2026-01-16 11:10:53 +0700

    commit Fri 01/16/2026 11:10:53.18

--- pages/product/index.vue
+++ pages/product/index.vue
@@ -3,6 +3,7 @@
     class="product-container pb-16 bg-grey-lighten-5"
     style="min-height: 100vh"
   >
+    <Header />
     <!-- Filter Section -->
     <div class="px-4 pt-4">
       <v-card elevation="0" rounded="xl" color="white" class="mb-4 border-0">
