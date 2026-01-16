e4ec756ba8726910783d9d238e60342365da57d1
huevangxp <huevang933@gmail.com>
2026-01-16 10:25:31 +0700

    commit Fri 01/16/2026 10:25:31.20

--- utils/hiddenOrderID.ts
+++ utils/hiddenOrderID.ts
@@ -1,4 +1,4 @@
-export default (orderID: string) => {
+export default function hiddenOrderID(orderID: string) {
   // slice first 2 and last 2 characters
   return orderID.slice(0, 2) + "XXX" + orderID.slice(-2);
-};
+}
