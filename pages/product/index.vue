4dc40120b67e722c6d91f92d58765fbdb192b45a
huevangxp <huevang933@gmail.com>
2026-01-16 11:11:05 +0700

    commit Fri 01/16/2026 11:11:05.15

--- pages/product/index.vue
+++ pages/product/index.vue
@@ -6,7 +6,7 @@
     <Header />
     <!-- Filter Section -->
     <div class="px-4 pt-4">
-      <v-card elevation="0" rounded="xl" color="white" class="mb-4 border-0">
+      <v-card elevation="0" rounded="xl" color="white" class="my-4 border-0">
         <v-card-text class="pa-4">
           <v-row dense>
             <v-col cols="8" sm="8">
