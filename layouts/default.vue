b8289bc5075f01cabea009173d64dced6e90d82b
huevangxp <huevang933@gmail.com>
2026-01-16 09:59:49 +0700

    commit Fri 01/16/2026  9:59:49.18

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,24 +1,22 @@
 <script setup lang="ts"></script>
 
 <template>
-<div>
-  <VitePwaManifest  />
-  <v-app>
-    <Header />
-    <v-main>
-      <slot />
-    </v-main>
-    <v-footer fixed app style="padding: 0px; margin: 0px;"> 
-      <Footer />
-    </v-footer>
-  </v-app>
-</div>
+  <div>
+    <VitePwaManifest />
+    <v-app>
+      <!-- <Header /> -->
+      <v-main>
+        <slot />
+      </v-main>
+      <v-footer fixed app style="padding: 0px; margin: 0px">
+        <Footer />
+      </v-footer>
+    </v-app>
+  </div>
 </template>
 
 <style>
-
-*{
+* {
   font-family: "Noto Sans Lao", sans-serif;
 }
-
 </style>
