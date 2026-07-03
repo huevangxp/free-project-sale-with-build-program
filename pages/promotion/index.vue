6cab30bfa5ac56d9933c5ca15de33818627f3e64
huevangxp <huevang933@gmail.com>
2026-07-03 14:45:48 +0700

    commit Fri 07/03/2026 14:45:48.15

--- pages/promotion/index.vue
+++ pages/promotion/index.vue
@@ -9,16 +9,39 @@
     </div>
 
     <v-container fluid class="px-4 mt-4">
-      <div class="d-flex align-center justify-space-between mb-4">
-        <div class="d-flex align-center">
-          <v-icon color="primary" class="mr-2">mdi-image-multiple</v-icon>
+      <!-- Section header -->
+      <div class="d-flex align-center mb-4">
+        <v-sheet
+          color="primary"
+          rounded="lg"
+          class="d-flex align-center justify-center mr-3"
+          height="40"
+          width="40"
+        >
+          <v-icon color="white">mdi-image-multiple</v-icon>
+        </v-sheet>
+        <div>
           <h2 class="text-h6 font-weight-bold text-primary">
             <span>ຮູບໂຄສະນາ</span>
           </h2>
+          <span class="text-caption text-medium-emphasis">
+            {{ promotions.length }} ຮູບພາບ
+          </span>
         </div>
       </div>
 
-      <v-row dense>
+      <!-- Loading skeletons -->
+      <v-row v-if="loading && promotions.length === 0" dense>
+        <v-col cols="12" sm="6" md="4" lg="3" v-for="n in 8" :key="n">
+          <v-skeleton-loader
+            type="image"
+            class="rounded-xl"
+          ></v-skeleton-loader>
+        </v-col>
+      </v-row>
+
+      <!-- Grid -->
+      <v-row v-else dense>
         <v-col
           cols="12"
           sm="6"
@@ -30,65 +53,71 @@
           <v-hover v-slot="{ isHovering, props }">
             <v-card
               v-bind="props"
-              class="promotion-card rounded-xl fill-height border-0"
-              elevation="0"
+              class="promotion-card rounded-xl overflow-hidden"
+              :elevation="isHovering ? 6 : 0"
               color="white"
             >
-              <div class="position-relative pa-2">
-                <v-card
-                  class="rounded-xl bg-grey-lighten-4 overflow-hidden"
-                  elevation="0"
+              <div class="position-relative">
+                <v-img
+                  :src="'http://localhost:8000/' + item.image"
+                  aspect-ratio="1"
+                  cover
+                  class="promotion-image"
+                  :class="{ 'zoom-effect': isHovering }"
                 >
-                  <v-img
-                    :src="'http://localhost:8000/' + item.image"
-                    aspect-ratio="1"
-                    cover
-                    class="promotion-image"
-                    :class="{ 'zoom-effect': isHovering }"
-                  >
-                    <template v-slot:placeholder>
-                      <div
-                        class="d-flex align-center justify-center fill-height bg-grey-lighten-4"
-                      >
-                        <v-progress-circular
-                          indeterminate
-                          color="primary"
-                          size="24"
-                        ></v-progress-circular>
-                      </div>
-                    </template>
-                  </v-img>
-                </v-card>
+                  <template v-slot:placeholder>
+                    <div
+                      class="d-flex align-center justify-center fill-height bg-grey-lighten-4"
+                    >
+                      <v-progress-circular
+                        indeterminate
+                        color="primary"
+                        size="24"
+                      ></v-progress-circular>
+                    </div>
+                  </template>
 
+                  <!-- Bottom gradient + like count overlay -->
+                  <div class="promotion-overlay d-flex align-end pa-3">
+                    <v-chip
+                      color="white"
+                      size="small"
+                      class="font-weight-bold"
+                      label
+                    >
+                      <v-icon start size="16" color="error">mdi-heart</v-icon>
+                      29
+                    </v-chip>
+                  </div>
+                </v-img>
+
+                <!-- Download button -->
                 <v-btn
                   icon
                   variant="flat"
                   color="white"
                   size="small"
-                  class="position-absolute top-0 right-0 mt-4 mr-4 rounded-circle"
+                  class="position-absolute top-0 right-0 ma-3 rounded-circle"
                   style="z-index: 2"
-                  elevation="1"
+                  elevation="2"
                   @click.stop="downloadImage(item.image)"
                 >
                   <v-icon color="primary" size="20">mdi-download</v-icon>
                 </v-btn>
               </div>
-
-              <div class="px-3 pb-3 pt-1">
-                <div class="d-flex align-center">
-                  <v-icon color="error" size="20" class="mr-1"
-                    >mdi-heart</v-icon
-                  >
-                  <span
-                    class="text-subtitle-2 font-weight-bold text-grey-darken-1"
-                    >29</span
-                  >
-                </div>
-              </div>
             </v-card>
           </v-hover>
         </v-col>
       </v-row>
+
+      <!-- Empty state -->
+      <div
+        v-if="!loading && promotions.length === 0"
+        class="text-center py-16 text-medium-emphasis"
+      >
+        <v-icon size="64" color="grey-lighten-1">mdi-image-off-outline</v-icon>
+        <p class="text-body-2 mt-2">ຍັງບໍ່ມີຮູບໂຄສະນາ</p>
+      </div>
     </v-container>
   </div>
 </template>
