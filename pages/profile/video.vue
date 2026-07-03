d767a8eb86c9c6fdf5e0d61af1f9de6f4ff2dd62
huevangxp <huevang933@gmail.com>
2026-07-03 15:40:51 +0700

    commit Fri 07/03/2026 15:40:51.17

--- pages/profile/video.vue
+++ pages/profile/video.vue
@@ -148,7 +148,7 @@
           <div class="d-flex align-center min-width-0">
             <v-icon class="mr-2">mdi-play-circle</v-icon>
             <span class="text-subtitle-1 font-weight-bold text-truncate">
-           <div>   {{ link.title || "ວິດີໂອ" }}</div>
+              {{ link.title || "ວິດີໂອ" }}
             </span>
           </div>
           <v-btn
